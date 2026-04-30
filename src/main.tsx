7b5f5a45ed5edac000d5f0c4afdbc319fd10a8db
khadidjabensallah <bensalahk305@gmail.com>
2026-04-30 23:25:43 +0200

    fix: forcefully override lovable title

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import logoImage from "@/assets/logo.png";
+
+// Force overwrite Lovable's meta tags
+document.title = "ديوان الصنعة | Diwan Al San'ah";
+const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (link) {
+    link.href = logoImage;
+} else {
+    const newLink = document.createElement('link');
+    newLink.rel = 'icon';
+    newLink.href = logoImage;
+    document.head.appendChild(newLink);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
